eadbc6859a051b56e3d49535dab2b35b65dfae9b
MaxwellBrohm <160036477+MaxwellBrohm@users.noreply.github.com>
2025-02-22 21:27:40 -0500

    Update main.cpp
    added a bunch of stuff bc i was bored
    still some stuff I gotta fix but its almost done

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 #include <conio.h>   // For _getch()
 #include <cstdlib>   // For system("cls")
 #include <ctime>     // For seeding randomness
+#include <string>
+#include <thread> // for animation
+#include <chrono> // for animation
 
 using namespace std;
+using namespace this_thread;
+using namespace chrono;
 
 // ANSI escape sequence for setting color
 #define SET_COLOR(r, g, b) "\033[38;2;" << r << ";" << g << ";" << b << "m"
@@ -13,7 +18,7 @@ using namespace std;
 // Game settings
 const int gridSize = 9;
 const int numOfBombs = 10;
-int spawnX = gridSize / 2, spawnY =  gridSize/2; // Player spawn position
+int spawnX = gridSize / 2, spawnY = gridSize / 2; // Player spawn position
 int pixelsPerIndex = 1; // Determines the size of each tile when rendered
 char playerIcon = '@';
 
@@ -45,6 +50,7 @@ Tile flag = Tile(false, 255, 0, 0, false, "#");
 
 // Game map
 vector<vector<Tile>> gameMap(gridSize, vector<Tile>(gridSize));
+vector<vector<Tile>> gameMapDupe(gridSize, vector<Tile>(gridSize));
 
 // Player position
 int playerX = spawnX, playerY = spawnY;
@@ -69,10 +75,20 @@ void printGrid() {
             cout << endl;
         }
     }
+    cout << "\n";
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridSize; j++) {
+            cout << SET_COLOR(gameMapDupe[i][j].colorR, gameMapDupe[i][j].colorG, gameMapDupe[i][j].colorB)
+                << gameMapDupe[i][j].texture << gameMapDupe[i][j].texture
+                << RESET_COLOR;
+        }
+        cout << endl;
+    }
 }
 
 // Function to handle player movement
-void movePlayer(char input) {
+bool movePlayer(char input) {
+    bool gameOver = false;
     int newX = playerX, newY = playerY;
 
     switch (input) {
@@ -90,10 +106,10 @@ void movePlayer(char input) {
         }
 
         if (gameMap[newY][newX].killsPlayer) {
-            playerX = spawnX;
-            playerY = spawnY;
+            gameOver = true;
         }
     }
+    return gameOver;
 }
 
 // Function to initialize game map
@@ -107,18 +123,206 @@ void initializeGame() {
     for (int i = 0; i < numOfBombs; i++) {
         randX = rand() % gridSize;
         randY = rand() % gridSize;
-        if (randX == 0 && randY == 0)
+        if (randX == 0 && randY == 0) {
             gameMap[randX][randY] = tile;
-        else
+            gameMapDupe[randX][randY] = tile;
+        }
+        else {
             gameMap[randX][randY] = bomb;
+            gameMapDupe[randX][randY] = bomb;
+        }
+            
     }
 }
 
+int checkFlagPlace(char input, int bombsLeft) {
+    if (input == '7') {
+        if (playerY != 0 && playerX != 0) {
+            if (gameMap[playerY - 1][playerX - 1] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY - 1][playerX - 1] == flag) {
+                if (gameMapDupe[playerY - 1][playerX - 1] == bomb)
+                    bombsLeft++;
+                gameMap[playerY - 1][playerX - 1] = gameMapDupe[playerY - 1][playerX - 1];
+            }
+            else
+                gameMap[playerY - 1][playerX - 1] = flag;
+        }
+    }
+    if (input == '8') {
+        if (playerY != 0) {
+            if (gameMap[playerY - 1][playerX] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY - 1][playerX] == flag) {
+                if (gameMapDupe[playerY - 1][playerX] == bomb)
+                    bombsLeft++;
+                gameMap[playerY - 1][playerX] = gameMapDupe[playerY - 1][playerX];
+            }
+            else
+                gameMap[playerY - 1][playerX] = flag;
+        }
+    }
+    if (input == '9') {
+        if (playerY != 0 && playerX != gridSize - 1) {
+            if (gameMap[playerY - 1][playerX + 1] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY - 1][playerX + 1] == flag) {
+                if (gameMapDupe[playerY - 1][playerX + 1] == bomb)
+                    bombsLeft++;
+                gameMap[playerY - 1][playerX + 1] = gameMapDupe[playerY - 1][playerX + 1];
+            }
+            else
+                gameMap[playerY - 1][playerX + 1] = flag;
+        }
+    }
+    if (input == '4') {
+        if (playerX != 0) {
+            if (gameMap[playerY][playerX - 1] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY][playerX - 1] == flag) {
+                if (gameMapDupe[playerY][playerX - 1] == bomb)
+                    bombsLeft++;
+                gameMap[playerY][playerX - 1] = gameMapDupe[playerY][playerX - 1];
+            }
+            else
+                gameMap[playerY][playerX - 1] = flag;
+        }
+    }
+    if (input == '6') {
+        if (playerX != gridSize - 1) {
+            if (gameMap[playerY][playerX + 1] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY][playerX + 1] == flag) {
+                if (gameMapDupe[playerY][playerX + 1] == bomb)
+                    bombsLeft++;
+                gameMap[playerY][playerX + 1] = gameMapDupe[playerY][playerX + 1];
+            }
+            else
+                gameMap[playerY][playerX + 1] = flag;
+        }
+    }
+    if (input == '1') {
+        if (playerY != gridSize - 1 && playerX != 0) {
+            if (gameMap[playerY + 1][playerX - 1] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY + 1][playerX - 1] == flag) {
+                if (gameMapDupe[playerY + 1][playerX - 1] == bomb)
+                    bombsLeft++;
+                gameMap[playerY + 1][playerX - 1] = gameMapDupe[playerY + 1][playerX - 1];
+            }
+            else
+                gameMap[playerY + 1][playerX - 1] = flag;
+        }
+    }
+    if (input == '2') {
+        if (playerY != gridSize - 1) {
+            if (gameMap[playerY + 1][playerX] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY + 1][playerX] == flag) {
+                if (gameMapDupe[playerY + 1][playerX] == bomb)
+                    bombsLeft++;
+                gameMap[playerY + 1][playerX] = gameMapDupe[playerY + 1][playerX];
+            }
+            else
+                gameMap[playerY + 1][playerX] = flag;
+        }
+    }
+    if (input == '3') {
+        if (playerY != gridSize - 1 && playerX != gridSize - 1) {
+            if (gameMap[playerY + 1][playerX + 1] == bomb)
+                bombsLeft--;
+            if (gameMap[playerY + 1][playerX + 1] == flag) {
+                if (gameMapDupe[playerY + 1][playerX + 1] == bomb)
+                    bombsLeft++;
+                gameMap[playerY + 1][playerX + 1] = gameMapDupe[playerY + 1][playerX + 1];
+            }
+            else
+                gameMap[playerY + 1][playerX + 1] = flag;
+        }
+    }
+    return bombsLeft;
+}
+
+
+int getSurroundingBombs(int y, int x) {
+    int surroundingBombs = 0;
+        if (y != 0 && x != 0) {
+            if (gameMap[y - 1][playerX - 1] == bomb)
+                surroundingBombs++;
+        }
+        if (y != 0) {
+            if (gameMap[y - 1][playerX] == bomb)
+                surroundingBombs++;
+        }
+        if (y != 0 && playerX != gridSize - 1) {
+            if (gameMap[y - 1][playerX + 1] == bomb)
+                surroundingBombs++; 
+        }
+        if (playerX != 0) {
+            if (gameMap[y][playerX - 1] == bomb)
+                surroundingBombs++; 
+        }
+        if (playerX != gridSize - 1) {
+            if (gameMap[y][playerX + 1] == bomb)
+                surroundingBombs++; 
+        }
+        if (y != gridSize - 1 && playerX != 0) {
+            if (gameMap[y + 1][playerX - 1] == bomb)
+                surroundingBombs++; 
+        }
+        if (y != gridSize - 1) {
+            if (gameMap[y + 1][playerX] == bomb)
+                surroundingBombs++; 
+        }
+        if (y != gridSize - 1 && playerX != gridSize - 1) {
+            if (gameMap[y + 1][playerX + 1] == bomb)
+                surroundingBombs++; 
+        }
+        return surroundingBombs;
+}
+
+void explosionAnimation() {
+    int waitTime = 100;
+
+    cout << "    )\n     (\n    .-`-.\n    :   :\n    :TNT:\n    :___:" << endl;
+    sleep_for(milliseconds(1000));
+    system("cls");
+
+    cout << "    \\|/\n   - o -\n    /-`-.\n    :   :\n    :TNT:\n    :___:" << endl;
+    sleep_for(milliseconds(1500));
+    system("cls");
+
+    cout << "\n\n    .---.\n    : | :\n    :-o-:\n    :_|_:" << endl;
+    sleep_for(milliseconds(waitTime));
+    system("cls");
+
+    cout << "\n\n    .---.\n    (\\|/)\n    --0--\n    (/|\\)" << endl;
+    sleep_for(milliseconds(waitTime));
+    system("cls");
+
+    cout << "\n\n   '.\\|/.'\n   (\\   /)\n   - -O- -\n   (/   \\)\n   ,'/'|\\'." << endl;
+    sleep_for(milliseconds(waitTime));
+    system("cls");
+
+    cout << "\n'.  \\ | /  ,'\n  `. `.' ,'\n ( .`.|,' .)\n - ~ -0- ~ -\n (\n','|'.` )\n  .' .'. '.\n,\\'  / | \\  '.\n" << endl;
+    sleep_for(milliseconds(waitTime));
+    system("cls");
+
+    cout << "\n    \\ '  \"  \n ` . `.' ,'\n . `` ,'. \"\n   ~ (   ~ -\n'\n. ','|` ` .\n  .'  \"  '\n,   ' , '  `" << endl;
+    sleep_for(milliseconds(waitTime));
+    system("cls");
+
+    cout << "\n\n   (  ) (\n    ) ( )\n    (  )\n     ) /\n    ,---." << endl;
+    sleep_for(milliseconds(waitTime));
+}
+
+
 int main() {
     initializeGame();
-    int bombsLeft = 1;
-    for (int i = 0; i < gridSize - 1; i++) {
-        for (int j = 0; j < gridSize - 1; j++) {
+
+    int bombsLeft = 0;
+    for (int i = 0; i <= gridSize - 1; i++) {
+        for (int j = 0; j <= gridSize - 1; j++) {
             if (gameMap[i][j] == bomb)
                 bombsLeft++;
         }
@@ -131,68 +335,22 @@ int main() {
         cout << "Number of bombs left:" << bombsLeft << "\n";
 
 
+        gameMap[playerY][playerX] = Tile(true, 255, 255 - (getSurroundingBombs(playerY, playerX) * 31), 255 - (getSurroundingBombs(playerY, playerX) * 31), false, to_string(getSurroundingBombs(playerY, playerX)));
+
+
         char input = _getch(); // Get user input
 
 
         if (input == 'q') break;
-        if (input == '7') {
-            if (playerY != 0 && playerX != 0) {
-                if (gameMap[playerY - 1][playerX - 1] == bomb)
-                    bombsLeft--;
-                gameMap[playerY - 1][playerX - 1] = flag;
-            }
-        }
-        if (input == '8') {
-            if (playerY != 0) {
-                if (gameMap[playerY - 1][playerX] == bomb)
-                    bombsLeft--;
-                gameMap[playerY - 1][playerX] = flag;
-            }
-        }
-        if (input == '9') {
-            if (playerY != 0 && playerX != gridSize - 1) {
-                if (gameMap[playerY - 1][playerX + 1] == bomb)
-                    bombsLeft--;
-                gameMap[playerY - 1][playerX + 1] = flag;
-            }
-        }
-        if (input == '4') {
-            if (playerX != 0) {
-                if (gameMap[playerY][playerX - 1] == bomb)
-                    bombsLeft--;
-                gameMap[playerY][playerX - 1] = flag;
-            }
-        }
-        if (input == '6') {
-            if (playerX != gridSize - 1) {
-                if (gameMap[playerY][playerX + 1] == bomb)
-                    bombsLeft--;
-                gameMap[playerY][playerX + 1] = flag;
-            }
-        }
-        if (input == '1') {
-            if (playerY != gridSize - 1 && playerX != 0) {
-                if (gameMap[playerY + 1][playerX - 1] == bomb)
-                    bombsLeft--;
-                gameMap[playerY + 1][playerX - 1] = flag;
-            }
-        }
-        if (input == '2') {
-            if (playerY != gridSize - 1) {
-                if (gameMap[playerY + 1][playerX] == bomb)
-                    bombsLeft--;
-                gameMap[playerY + 1][playerX] = flag;
-            }
-        }
-        if (input == '3') {
-            if (playerY != gridSize - 1 && playerX != gridSize - 1) {
-                if (gameMap[playerY + 1][playerX + 1] == bomb)
-                    bombsLeft--;
-                gameMap[playerY + 1][playerX + 1] = flag;
-            }
-        }
+        bombsLeft = checkFlagPlace(input, bombsLeft);
+        
 
-        movePlayer(input);
+        if (movePlayer(input)) {
+            system("cls");
+            explosionAnimation();
+            cout << "You hit a bomb!!";
+            break;
+        }
     }
 
     return 0;
